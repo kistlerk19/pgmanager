f26ae8ea33344ca79332abddbeed6fe5fe7d746a
Ishmael Gyamfi <isgyamfi25@gmail.com>
2025-03-01 16:05:31 +0000

    search filtering

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,9 +1,27 @@
 import Pagination from '@/Components/Pagination';
+import SelectInput from '@/Components/SelectInput';
+import TextInput from '@/Components/TextInput';
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from '@/constants';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head, Link } from '@inertiajs/react';
+import { Head, Link, router } from '@inertiajs/react';
 
-export default function Index({ auth, projects }) {
+export default function Index({ auth, projects, queryParams = null }) {
+    queryParams = queryParams || {};
+    const searchFieldChanged = (name, value) => {
+        if (value) {
+            queryParams[name] = value;
+        } else {
+            delete queryParams[name];
+        }
+
+        router.get(route('project.index'), queryParams);
+    };
+
+    const onKeyPress = (name, e) => {
+        if (e.key !== 'Enter') return;
+
+        searchFieldChanged(name, e.target.value);
+    };
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -21,6 +39,57 @@ export default function Index({ auth, projects }) {
                             <table className="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
                                 <thead className="text-xs text-gray-700 uppercase border-b-2 border-gray-500 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr className="text-nowrap">
+                                        <th className="px-3 py-3"></th>
+                                        <th className="px-3 py-3"></th>
+                                        <th className="px-3 py-3">
+                                            <TextInput
+                                                defaultValue={queryParams.name}
+                                                className="w-full"
+                                                placeholder="Project Name"
+                                                onBlur={(e) =>
+                                                    searchFieldChanged(
+                                                        'name',
+                                                        e.target.value,
+                                                    )
+                                                }
+                                                onKeyPress={(e) =>
+                                                    onKeyPress('name', e)
+                                                }
+                                            />
+                                        </th>
+                                        <th className="px-3 py-3">
+                                            <SelectInput
+                                                defaultValue={queryParams.status}
+                                                className="w-full"
+                                                onChange={(e) =>
+                                                    searchFieldChanged(
+                                                        'status',
+                                                        e.target.value,
+                                                    )
+                                                }
+                                            >
+                                                <option value="">
+                                                    Select Status
+                                                </option>
+                                                <option value="pending">
+                                                    Pending
+                                                </option>
+                                                <option value="in_progress">
+                                                    In Progress
+                                                </option>
+                                                <option value="completed">
+                                                    Completed
+                                                </option>
+                                            </SelectInput>
+                                        </th>
+                                        <th className="px-3 py-3"></th>
+                                        <th className="px-3 py-3"></th>
+                                        <th className="px-3 py-3"></th>
+                                        <th className="px-3 py-3 text-center"></th>
+                                    </tr>
+                                </thead>
+                                <thead className="text-xs text-gray-700 uppercase border-b-2 border-gray-500 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr className="text-nowrap">
                                         <th className="px-3 py-3">ID</th>
                                         <th className="px-3 py-3">Image</th>
                                         <th className="px-3 py-3">Name</th>
@@ -32,17 +101,23 @@ export default function Index({ auth, projects }) {
                                         <th className="px-3 py-3">
                                             Created By
                                         </th>
-                                        <th className="px-3 py-3 text-center">Actions</th>
+                                        <th className="px-3 py-3 text-center">
+                                            Actions
+                                        </th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {projects.data.map((project) => (
-                                        <tr key={project.id} className="bg-white border-b dark:border-gray-800 dark:bg-gray-800">
+                                        <tr
+                                            key={project.id}
+                                            className="bg-white border-b dark:border-gray-800 dark:bg-gray-800"
+                                        >
                                             <td className="px-3 py-2">
                                                 {project.id}
                                             </td>
                                             <td className="px-3 py-2">
-                                                <img style={{ width: 60 }}
+                                                <img
+                                                    style={{ width: 60 }}
                                                     src={project.image_path}
                                                     alt=""
                                                 />
@@ -53,10 +128,18 @@ export default function Index({ auth, projects }) {
                                             <td className="px-3 py-2">
                                                 <span
                                                     className={
-                                                        "px-2 py-1 rounded text-white " +
-                                                        PROJECT_STATUS_CLASS_MAP[project.status]
+                                                        'rounded px-2 py-1 text-white ' +
+                                                        PROJECT_STATUS_CLASS_MAP[
+                                                            project.status
+                                                        ]
                                                     }
-                                                >{PROJECT_STATUS_TEXT_MAP[project.status]}</span>
+                                                >
+                                                    {
+                                                        PROJECT_STATUS_TEXT_MAP[
+                                                            project.status
+                                                        ]
+                                                    }
+                                                </span>
                                             </td>
                                             <td className="px-3 py-2 text-nowrap">
                                                 {project.created_at}
@@ -68,10 +151,22 @@ export default function Index({ auth, projects }) {
                                                 {project.createdBy.name}
                                             </td>
                                             <td className="px-3 py-2 text-center">
-                                                <Link className="mx-1 font-medium text-blue-600 dark:text-blue-500 hover:underline" href={route('project.edit', project.id)}>
+                                                <Link
+                                                    className="mx-1 font-medium text-blue-600 hover:underline dark:text-blue-500"
+                                                    href={route(
+                                                        'project.edit',
+                                                        project.id,
+                                                    )}
+                                                >
                                                     Edit
                                                 </Link>
-                                                <Link className="mx-1 font-medium text-red-600 dark:text-red-500 hover:underline" href={route('project.destroy', project.id)}>
+                                                <Link
+                                                    className="mx-1 font-medium text-red-600 hover:underline dark:text-red-500"
+                                                    href={route(
+                                                        'project.destroy',
+                                                        project.id,
+                                                    )}
+                                                >
                                                     Delete
                                                 </Link>
                                             </td>
